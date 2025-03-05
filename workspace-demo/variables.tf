2916815c1acb0bf4c2397a72717fafc0e3a8db7a
lokeshburle <138304043+lokeshburle@users.noreply.github.com>
2025-03-05 12:05:18 +0530

    Update variables.tf

---
+++ workspace-demo/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_type" {
+    type = string
+    description = "AMI ID for ec2 instance"
+  
+}
+variable "instance_type" {
+    type = string
+    description = "Typ of ec2 instance"
+  
+}
+variable "keyname" {
+    type = string
+    
+
+  
+}
